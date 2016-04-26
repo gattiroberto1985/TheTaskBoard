077cb6d8431f7c73a2e9f2bdcfdbef9bc4903cc9
debianbob <a@b.c>
2016-04-26 21:01:21 +0200

    Update con servizio per gestione progetto. Questa parte sembra andare, rimane da gestire tutta la parte backend. E la grafica, ovviamente.....

--- scripts/controllers/projectsCtrl.js
+++ scripts/controllers/projectsCtrl.js
@@ -1,12 +1,16 @@
-app.controller("projectsCtrl", function ( $scope, $filter, $location ) {
+app.controller("projectsCtrl", function ( $scope, $filter, $location, projectServ ) {
     console.log(" [ AJS ] [ projectsCtrl ] Entering projects controller . . .");
 
     var timelineSortField   = $scope.timelineSortField = 'date';
     var timelineSortSense   = $scope.timelineSortSense = false;
 
     // Contains a new project
-    var newProject      = $scope.newProject      = null;
+    $scope.newProject      = null;
 
+    $scope.projects = projectServ.projects;
+    $scope.owners   = projectServ.owners;
+    $scope.statuses = projectServ.statuses;
+    
     // It represents the selected project. A backup copy is provided via
     // id of the project in the $scope.projects array.
     var sProject        = $scope.sProject        = null;
@@ -18,116 +22,26 @@ app.controller("projectsCtrl", function ( $scope, $filter, $location ) {
     //var originalProject = $scope.originalProject = null;
     // Contains the edit to the project
     //var editedProject   = $scope.editedProject   = null;
-    /**
-     * TODO: This datas must be retreived from a database layer!
-     */
-    var owners = $scope.owners = [
-        { id: "unknown"        , value: "Non Assegnato!" },
-        { id: "roberto.gatti"  , value: "Roberto"        },
-        { id: "stefano.moretto", value: "Stefano M."     },
-        { id: "s.fiorini"      , value: "Stefano F."     }
-    ];
-
-    /**
-     * TODO: This datas must be retreived from a database layer!
-     */
-    var statuses = $scope.statuses = [
-        { id: 0, value: "Aperto"         },
-        { id: 1, value: "In Lavorazione" },
-        { id: 2, value: "In Pausa"       },
-        { id: 3, value: "Chiuso"         }
-    ];
-
-    // TODO: this datas MUST be retrieved from a database layer!
-    var projects = $scope.projects = [
-        {
-            id: getUid(),
-            title: "Progetto 1",
-            description: "Descrizione progetto 1",
-            status: statuses[0],
-            owner: owners[1],
-
-        },
-        {
-            id: getUid(),
-            title: "Progetto 2",
-            description: "Descrizione progetto 2",
-            status: statuses[1],
-            owner: owners[1]
-        },
-        {
-            id: getUid(),
-            title: "Progetto 3",
-            description: "Descrizione progetto 3",
-            status: statuses[2],
-            owner: owners[3],
-            dateOpen: new Date(2016, 04, 02),
-            dateClose: null,
-            dateLastUpdated: new Date(2016, 04, 24),
-            statusNote: "In attesa chiarimenti",
-            timeline: [
-                { date: new Date(2016, 04, 03), status: "In Lavorazione", note: "Inizio lavori!"},
-                { date: new Date(2016, 04, 06), status: "In Pausa"      , note: "Inviate indicazioni, in attesa riscontro"},
-                { date: new Date(2016, 04, 06), status: "In Lavorazione", note: "Info recuperate, riparto con l'attività"}
-            ],
-            notes: [
-                { date: new Date(2016, 04, 03, 12, 22, 00), title: "titolo nota 1", text: "testo nota 1 " },
-                { date: new Date(2016, 04, 07, 17, 48, 00), title: "titolo nota 2", text: "testo nota 2 " },
-                { date: new Date(2016, 04, 13, 14, 12, 00), title: "titolo nota 3", text: "testo nota 3 " }
-            ],
-            tasks: [
-                {
-                    id: getUid(),
-                    title: "Task 1",
-                    description: "Descrizione Task 1",
-                    status: "In Pausa",
-                    owner: owners[1],
-                    dateOpen: new Date(2016, 04, 04),
-                    dateClose: null,
-                    dateLastUpdated: new Date(2016, 04, 07),
-                    statusNote: "",
-                    notes: [
-                        { date: new Date(2016, 04, 04, 12, 00), title: "titolo nota task 1", text: "testo nota task 1"}
-                    ]
-                }
-            ]
-        }
-    ];
 
     /**
      * The method add a project in the store.
      */
     $scope.addProject = function ( ) {
-        // Calling API for inserting project and cleaning newProject var
-        console.log( " [ AJS ] [ projectsCtrl ] cleaning objects . . . ");
-
-        if ( $scope.newProject.status == null )
-            $scope.newProject.status = "Aperto";
-
-        if ( $scope.newProject.owner == null )
-            $scope.newProject.owner = owners[0];
-
-        $scope.newProject.dateOpen = $scope.newProject.dateLastUpdated = new Date();
-        $scope.newProject.dateClose = null;
-
-        $scope.newProject.id = getUid();
-
-        projects.push( $scope.newProject );
-        newProject = $scope.newProject = null;
+        projectServ.createProject ( $scope.newProject );
+        $scope.newProject = null;
     };
 
-    /* ********************************************************************
-     *                    VIEW MANAGER FUNCTION
-     * ******************************************************************** */
+    /* ******************************************************************** */
+    /*                        VIEW MANAGER FUNCTION                         */
+    /* ******************************************************************** */
 
     /**
      * This function opens the header of the project in the view.
      */
     $scope.openProject = function ( project ) {
         console.log(" [ AJS ] [ projectsCtrl ] Opening project . . .");
-        originalProject = angular.extend({ }, project);
+        projectServ.setSelectedProject( project );
         $scope.sProject = angular.extend({ }, project);
-
     };
 
     /**
@@ -150,7 +64,6 @@ app.controller("projectsCtrl", function ( $scope, $filter, $location ) {
     $scope.closeProject = function ( ) {
         console.log("Closing project");
         $scope.sProject = null;
-        originalProject = null;
     };
 
     /**
@@ -158,17 +71,17 @@ app.controller("projectsCtrl", function ( $scope, $filter, $location ) {
      */
     $scope.rollbackProject = function( ) {
         console.log("Rollbacking changes . . .");
-        $scope.sProject          = angular.extend({ }, originalProject);
+        $scope.sProject          = angular.extend({ }, projectServ.sProject);
         $scope.sProject.notes    = [ ];
         $scope.sProject.timeline = [ ];
         // Copying notes . . .
-        for (var i = 0; i < originalProject.notes.length; i++) {
-            var note =  originalProject.notes[i];
+        for (var i = 0; i < projectServ.sProject.notes.length; i++) {
+            var note =  projectServ.sProject.notes[i];
             $scope.sProject.notes.push(angular.extend( { }, note ));
         }
         // Copying timeline . . .
-        for (var i = 0; i < originalProject.timeline.length; i++) {
-            var timestep =  originalProject.timeline[i];
+        for (var i = 0; i < projectServ.sProject.timeline.length; i++) {
+            var timestep =  projectServ.sProject.timeline[i];
             $scope.sProject.timeline.push(angular.extend( { }, timestep ));
         }
     };
@@ -177,18 +90,8 @@ app.controller("projectsCtrl", function ( $scope, $filter, $location ) {
      *
      */
     $scope.removeProject = function ( projectId ) {
-        console.log(" [ AJS ] [ projectsCtrl ] deleting project with id '" + projectId + "'. . . ");
-        var i = 0;
-        for ( i = 0; i < projects.length; i++ )
-        {
-            if ( projects[i].id == projectId )
-            {
-                projects.splice(i, 1 );
-                break;
-            }
-        }
-        $scope.sProject = originalProject = null;
-        console.log( "Removed project at index '" + i + "'");
+        projectServ.removeProject ( projectId );
+        $scope.sProject = projectServ.sProject = null;
     };
 
 
@@ -202,6 +105,9 @@ app.controller("projectsCtrl", function ( $scope, $filter, $location ) {
         console.log(" [ AJS ] [ projectsCtrl ] Adding note to project . . . ");
         $scope.newNote.date = new Date();
         $scope.newNote.title = note.text.substring(10);
+
+        if ( $scope.sProject.notes === undefined )
+            $scope.sProject.notes = [ ];
 
         $scope.sProject.notes.push($scope.newNote);
         $scope.newNote = newNote = null;
@@ -230,18 +136,9 @@ app.controller("projectsCtrl", function ( $scope, $filter, $location ) {
     /**
      *
      */
-    /*$scope.updateProject = function ( project ) {
-        // Checking project fields ...
-        store['put'](project)
-            .then(function success() {}, function error() {
-                project = $scope.originalProject;
-            })
-            .finally(function () {
-                $scope.editedProject = null;
-            });
-            console.log("Updating project . . .");
-            $scope.originalProject = null;
-            $scope.editedProject = null;
-    };*/
+    $scope.updateProject = function ( updatedProject ) {
+        console.log(" [ AJS ] [ projectsCtrl ] Trying to save the project . . .");
+        projectServ.saveProject( updatedProject );
+    };
 
 });
--- scripts/providers/projectServ.js
+++ scripts/providers/projectServ.js
@@ -1,27 +1,158 @@
-app.service('projectService', function ( ) {
+app.service('projectServ', function ( ) {
 
-    this.projects          = null;
+    /**
+     * TODO: This datas must be retreived from a database layer!
+     */
+    this.owners = [
+        { id: "unknown"        , value: "Non Assegnato!" },
+        { id: "roberto.gatti"  , value: "Roberto"        },
+        { id: "stefano.moretto", value: "Stefano M."     },
+        { id: "s.fiorini"      , value: "Stefano F."     }
+    ];
+
+    /**
+     * TODO: This datas must be retreived from a database layer!
+     */
+    this.statuses = [
+        { id: 0, value: "Aperto"         },
+        { id: 1, value: "In Lavorazione" },
+        { id: 2, value: "In Pausa"       },
+        { id: 3, value: "Chiuso"         }
+    ];
+
+    // TODO: this datas MUST be retrieved from a database layer!
+    this.projects = [
+        {
+            id: getUid(),
+            title: "Progetto 1",
+            description: "Descrizione progetto 1",
+            status: this.statuses[0],
+            owner: this.owners[1],
+
+        },
+        {
+            id: getUid(),
+            title: "Progetto 2",
+            description: "Descrizione progetto 2",
+            status: this.statuses[1],
+            owner: this.owners[1]
+        },
+        {
+            id: getUid(),
+            title: "Progetto 3",
+            description: "Descrizione progetto 3",
+            status: this.statuses[2],
+            owner: this.owners[3],
+            dateOpen: new Date(2016, 04, 02),
+            dateClose: null,
+            dateLastUpdated: new Date(2016, 04, 24),
+            statusNote: "In attesa chiarimenti",
+            timeline: [
+                { date: new Date(2016, 04, 03), status: "In Lavorazione", note: "Inizio lavori!"},
+                { date: new Date(2016, 04, 06), status: "In Pausa"      , note: "Inviate indicazioni, in attesa riscontro"},
+                { date: new Date(2016, 04, 06), status: "In Lavorazione", note: "Info recuperate, riparto con l'attività"}
+            ],
+            notes: [
+                { date: new Date(2016, 04, 03, 12, 22, 00), title: "titolo nota 1", text: "testo nota 1 " },
+                { date: new Date(2016, 04, 07, 17, 48, 00), title: "titolo nota 2", text: "testo nota 2 " },
+                { date: new Date(2016, 04, 13, 14, 12, 00), title: "titolo nota 3", text: "testo nota 3 " }
+            ],
+            tasks: [
+                {
+                    id: getUid(),
+                    title: "Task 1",
+                    description: "Descrizione Task 1",
+                    status: "In Pausa",
+                    owner: this.owners[1],
+                    dateOpen: new Date(2016, 04, 04),
+                    dateClose: null,
+                    dateLastUpdated: new Date(2016, 04, 07),
+                    statusNote: "",
+                    notes: [
+                        { date: new Date(2016, 04, 04, 12, 00), title: "titolo nota task 1", text: "testo nota task 1"}
+                    ]
+                }
+            ]
+        }
+    ];
+
     this.selectedProjectId = null;
     this.sProject          = null;
 
+    /* ********************************************************************* */
+    /*                                CRUD METHODS                           */
+    /* ********************************************************************* */
+
     this.createProject = function ( project )
     {
+        // Calling API for inserting project and cleaning newProject var
+        console.log( " [ AJS ] [ projectServ ] Creating project . . .");
 
+        if ( project.status == null )
+            project.status = "Aperto";
+
+        if ( project.owner == null )
+            project.owner = owners[0];
+
+        project.dateOpen = project.dateLastUpdated = new Date();
+        project.dateClose = null;
+
+        project.id = getUid();
+
+        this.projects.push( project );
+        this.selectedProjectId = project.id;
+        this.sProject = angular.extend( { }, project );
     };
 
     this.removeProject = function ( projectId )
     {
-
+        console.log(" [ AJS ] [ projectServ ] deleting project with id '" + projectId + "'. . . ");
+        var i = 0;
+        for ( i = 0; i < this.projects.length; i++ )
+        {
+            if ( this.projects[i].id == projectId )
+            {
+                this.projects.splice(i, 1 );
+                break;
+            }
+        }
+        console.log(" [ AJS ] [ projectServ ] Deleted project at index '" + i  + "' . . .");
     };
 
-    this.rollbackProject = function ( )
-    {
-
-    };
-
+    /**
+     * Starting from the id registered in the selectedProjectId field, an update
+     * of the project is tried.
+     */
     this.saveProject = function ( project )
     {
+        console.log("Updating project . . .");
+        for ( var i = 0; i < this.projects.length; i++ )
+        {
+            if ( this.projects[i].id == project.id )
+            {
+                this.projects[i] = project;
+                console.log("Project updated!");
+                return;
+            }
+        }
+        console.log(" ERROR: unable to find project to update with id: '" + project.id + "'");
+    };
 
+    /* ********************************************************************* */
+    /*                             OTHER METHODS                             */
+    /* ********************************************************************* */
+
+    this.setSelectedProject = function ( project )
+    {
+        // Mantaining a backup copy of the project, to have simple rollback
+        // behaviour...
+        this.sProject = angular.extend( { }, project );
+        if ( this.sProject.notes === undefined )
+            this.sProject.notes = [ ];
+        if ( this.sProject.timeline === undefined )
+                this.sProject.timeline = [ ];
+
+        console.log(" [ AJS ] [ projectServ ] selected project: '" + JSON.stringify(this.sProject) + "'");
     };
 
 });
