34650784541fbf0c527e7c619c60335abb9f8241
Roberto Gatti <roberto.gatti@workpc.it>
2017-12-01 19:24:43 +0100

    Rimozione file nodejs

--- server/models/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    user   : require('./user.js'),
-    fask   : require('./fask.js'),
-    project: require('./project.js')
-};
--- server/routes.js
+++
@@ -1,6 +0,0 @@
-// Assigning a controller to each path . . .
-module.exports = {
-    'user'   : require('./controllers/userCntr.js'),
-    'fask'   : require('./controllers/faskCntr.js'),
-    'project': require('./controllers/projectCtrl.js')
-};
